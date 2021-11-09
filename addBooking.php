e60b9f2747804b47bc36c98f2a50f9cccca2ca46
Declan Mould <44209534+TheGamerzs@users.noreply.github.com>
2021-11-09 13:35:40 +0000

    format

--- addBooking.php
+++ addBooking.php
@@ -1,63 +1,80 @@
-<?php
-require_once 'db.php';
-
-if (isset($_POST['add'])) {
-
-    $pitch = $_POST['pitch'];
-    $user = $_POST['user'];
-
-    $pitch = mysqli_real_escape_string($conn, $pitch);
-    $user = mysqli_real_escape_string($conn, $user);
-
-    $sql = "INSERT INTO booking (`time`, pitch, userid) VALUES (CURDATE(), $pitch, $user)";
-    $request = mysqli_query($conn, $sql);
-
-    if ($request) {
-        echo '<p>Booking Added</p>';
-    } else {
-        echo '<p>Booking could not be added</p>';
-    }
-}
-
-?>
-
-<html>
-
-<head>
-    <title>Booking Prototype</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
-</head>
-
-<body>
-    <form method="POST">
-        <div>
-            <label for="pitch">Pitch:</label>
-            <select name="pitch">
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
-        </div>
-        <div>
-
-            <label for="user">User:</label>
-            <select name="user">
-                <option value="456">456</option>
-                <option value="785">785</option>
-                <option value="786">786</option>
-                <option value="1024">1024</option>
-                <option value="2048">2048</option>
-            </select>
-        </div>
-        <div>
-            <input name="add" type="submit" value="Submit">
-        </div>
-    </form>
-
-    <a href="bookings.php">View bookings</a>
-    <a href="./">Home</a>
-</body>
-
+<?php
+
+require_once 'db.php';
+
+if (isset($_POST['add'])) {
+
+    $pitch = $_POST['pitch'];
+    $user = $_POST['user'];
+
+    $pitch = mysqli_real_escape_string($conn, $pitch);
+    $user = mysqli_real_escape_string($conn, $user);
+
+    $sql = "INSERT INTO booking (`time`, pitch, userid) VALUES (CURDATE(), $pitch, $user)";
+    $request = mysqli_query($conn, $sql);
+
+    if ($request) {
+        echo '<p>Booking Added</p>';
+    } else {
+        echo '<p>Booking could not be added</p>';
+    }
+}
+
+$sql = "SELECT pitchid FROM pitch";
+$request = mysqli_query($conn, $sql);
+
+$bookings = [];
+
+if ($results) {
+    while ($pitch = mysqli_fetch_array($results)) {
+        $pitches[] = $pitch;
+    }
+}
+
+?>
+
+<html>
+
+<head>
+    <title>Booking Prototype</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+
+    <form method="POST">
+        <div>
+            <label for="pitch">Pitch:</label>
+            <select name="pitch">
+                <?php
+                foreach ($pitches as $pitch) {
+                    echo '<option value="' . $pitch['pitchid'] . '">' . $pitch['pitchid'] . '</option>';
+                }
+                ?>
+            </select>
+        </div>
+        <div>
+
+            <label for="user">User:</label>
+            <select name="user">
+                <option value="456">456</option>
+                <option value="785">785</option>
+                <option value="786">786</option>
+                <option value="1024">1024</option>
+                <option value="2048">2048</option>
+            </select>
+        </div>
+
+        <div>
+            <input name="add" type="submit" value="Submit">
+        </div>
+    </form>
+
+    <a href="bookings.php">View bookings</a>
+    <a href="./">Home</a>
+
+</body>
+
+
+
 </html>
--- bookings.php
+++ bookings.php
@@ -1,48 +1,89 @@
-<?php
-require_once('db.php');
-
-$sql = "SELECT id, time, pitch, userid FROM `booking`";
-$results = mysqli_query($conn, $sql);
-
-$bookings = [];
-
-if ($results) {
-    while ($booking = mysqli_fetch_array($results)) {
-        $bookings[] = $booking;
-    }
-}
-?>
-<html>
-
-<head>
-    <title>Booking Prototype</title>
-    <link rel="stylesheet" type="text/css" href="bookings.css">
-</head>
-
-<body>
-    <table id="bookingsTable">
-        <tbody>
-            <tr>
-                <th>ID</th>
-                <th>Date</th>
-                <th>Pitch</th>
-                <th>UserID</th>
-            </tr>
-            <?php
-            foreach ($bookings as $booking) {
-                echo '<tr>
-                <td>' . $booking[0] . '</td>
-                <td>' . $booking[1] . '</td>
-                <td>' . $booking[2] . '</td>
-                <td>' . $booking[3] . '</td>
-            </tr>';
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <a href="addBooking.php">Add booking</a>
-    <a href="./">Home</a>
-</body>
-
+<?php
+
+require_once('db.php');
+
+$sql = "SELECT id, time, pitch, userid FROM `booking`";
+
+$results = mysqli_query($conn, $sql);
+
+$bookings = [];
+
+if ($results) {
+
+    while ($booking = mysqli_fetch_array($results)) {
+
+        $bookings[] = $booking;
+
+    }
+
+}
+
+?>
+
+<html>
+
+
+
+<head>
+
+    <title>Booking Prototype</title>
+
+    <link rel="stylesheet" type="text/css" href="bookings.css">
+
+</head>
+
+
+
+<body>
+
+    <table id="bookingsTable">
+
+        <tbody>
+
+            <tr>
+
+                <th>ID</th>
+
+                <th>Date</th>
+
+                <th>Pitch</th>
+
+                <th>UserID</th>
+
+            </tr>
+
+            <?php
+
+            foreach ($bookings as $booking) {
+
+                echo '<tr>
+
+                <td>' . $booking[0] . '</td>
+
+                <td>' . $booking[1] . '</td>
+
+                <td>' . $booking[2] . '</td>
+
+                <td>' . $booking[3] . '</td>
+
+            </tr>';
+
+            }
+
+            ?>
+
+        </tbody>
+
+    </table>
+
+
+
+    <a href="addBooking.php">Add booking</a>
+
+    <a href="./">Home</a>
+
+</body>
+
+
+
 </html>
