2bbac66e29decceca680015786a152a7c9d0d475
Declan Mould <44209534+TheGamerzs@users.noreply.github.com>
2021-11-23 14:17:55 +0000

    Update booking.sql

--- sql/booking.sql
+++ sql/booking.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `booking` (
     `bookingID` int NOT NULL AUTO_INCREMENT,
-    `time` varchar(25) NOT NULL,
+    `start` varchar(25) NOT NULL,
+    `end` varchar(25) NOT NULL,
     `pitch` int(2) NOT NULL,
     `userid` int(10) NOT NULL,
     PRIMARY KEY (`bookingID`)
